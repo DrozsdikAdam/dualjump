432d9b7367f74f753b01e0f8c5688dc1ca01a698
DrozsdikAdam <drozsdik17@gmail.com>
2025-09-19 17:32:28 +0200

    working functions and fixing the method names

--- extension.js
+++ extension.js
@@ -1,34 +1,108 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+var fileA = null;
+var fileB = null;
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dualjump" is now active!');
+	const MountFile = vscode.commands.registerCommand("dualjump.mountFile", () => {
+		const temp = vscode.window.activeTextEditor;
+		const path = temp ? temp.document.uri.fsPath : null;
+		if (temp) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('dualjump.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+			if (!fileA && fileB !== path) {
+				fileA = path;
+				vscode.window.showInformationMessage("File A mounted: " + fileA)
+			}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DualJump!');
+			if (!fileB && fileA !== path) {
+				fileB = path;
+				vscode.window.showInformationMessage("File B mounted: " + fileB)
+			}
+
+			if (fileA && fileB && fileA !== path && fileB !== path) {
+				vscode.window.showWarningMessage("Both files are already mounted! unmount one to mount a new file.");
+				return;
+			}
+
+			if (fileA && fileB) vscode.window.showInformationMessage("Both files mounted!")
+
+		} else {
+			vscode.window.showWarningMessage("No active file to mount!");
+			return;
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	const SwitchToFileA = vscode.commands.registerCommand("dualjump.SwitchToFileA", async () => {
+		const temp = vscode.window.activeTextEditor;
+		const path = temp ? temp.document.uri.fsPath : null;
+
+		if (!fileA) {
+			vscode.window.showWarningMessage("There is no file mounted here!")
+			return;
+		}
+
+		if (fileA === path) {
+			vscode.window.showWarningMessage("You are already in that file!")
+			return;
+		}
+
+		if (fileA) {
+			const doc = await vscode.workspace.openTextDocument(fileA);
+			await vscode.window.showTextDocument(doc, { preview: false })
+		}
+	})
+
+	const SwitchToFileB = vscode.commands.registerCommand("dualjump.SwitchToFileB", async () => {
+		const temp = vscode.window.activeTextEditor;
+		const path = temp ? temp.document.uri.fsPath : null;
+
+		if (!fileB) {
+			vscode.window.showWarningMessage("There is no file mounted here!")
+			return;
+		}
+
+		if (fileB === path) {
+			vscode.window.showWarningMessage("You are already in that file!")
+			return;
+		}
+
+		if (fileB) {
+			const doc = await vscode.workspace.openTextDocument(fileB);
+			await vscode.window.showTextDocument(doc, { preview: false })
+		}
+	})
+
+	const UnmountFile = vscode.commands.registerCommand("dualjump.unmountCurrentFile", () => {
+		const temp = vscode.window.activeTextEditor;
+		const path = temp ? temp.document.uri.fsPath : null;
+
+		if (!temp) {
+			vscode.window.showWarningMessage("No active file to unmount!");
+			return;
+		}
+
+		if (fileA === path) {
+			fileA = null;
+			vscode.window.showInformationMessage("The first file has been unmounted.");
+		}
+		else if (fileB === path) {
+			fileB = null;
+			vscode.window.showInformationMessage("The second file has been unmounted.");
+		} else {
+			vscode.window.showWarningMessage("The active file is not mounted.");
+			return;
+		}
+	})
+
+	context.subscriptions.push(MountFile, SwitchToFileA, SwitchToFileB, UnmountFile);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
